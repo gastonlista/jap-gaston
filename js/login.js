7de8c8540c8ce5424aa41d02eda3f5feae53b9c7
gastonlista <gastonorigin1@hotmail.com>
2022-08-10 17:39:50 -0300

    Updated login and product section

---
+++ js/login.js
@@ -0,0 +1,30 @@
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const button = document.getElementById("button")
+const label = document.getElementsByTagName("label")
+
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (username.value == "" || password.value == "") {
+        let myElm = document.createElement("p");
+        myElm.innerText = 'Ingresa tu e-mail';
+        myElm.style.color = 'red';
+        username.style.border="1px solid red"
+        let Elm = document.createElement("p");
+        Elm.innerText = 'Ingresa tu contraseña';
+        Elm.style.color = 'red';
+        password.style.border="1px solid red"
+        if (label[0].getElementsByTagName("p").length == 0) {
+            label[0].appendChild(myElm);
+        }
+        if (label[1].getElementsByTagName("p").length == 0) {
+            label[1].appendChild(Elm);
+        }
+       console.log(label[0].getElementsByTagName("p"))
+    }
+    else {
+        window.location.href = "https://gastonlista.github.io/jap-gaston/";
+    }
+})
+
+
---
+++ js/products.js
@@ -0,0 +1,33 @@
+let datos = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+
+fetch(datos).then
+    (respuesta => respuesta.json()).then(respuestadatos => {
+        for (let dato of respuestadatos.products) {
+            let creartabla = `
+     <div onclick="setCatID(101)" class="list-group-item list-group-item-action cursor-active">
+     <div class="row">
+         <div class="col-3">
+             <img src="`+dato.image+`" alt="Los mejores precios en autos 0 kilómetro, de alta y media gama." class="img-thumbnail">
+         </div>
+         <div class="col">
+             <div class="d-flex w-100 justify-content-between">
+                 <h4 class="mb-1">`+dato.name+`  -  `+dato.currency+` `+dato.cost+`</h4>
+                 <small class="text-muted">`+dato.soldCount+` articulos</small>
+             </div>
+             <p class="mb-1">`+dato.description+`</p>
+         </div>
+     </div>
+ </div>
+ `;
+ document.getElementById(productosautos.innerHTML += creartabla)
+ console.log(respuestadatos.products)
+        }
+    })
+
+
+
+
+
+
+
+
